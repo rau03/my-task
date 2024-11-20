d00b660ddaf2df2e302a672e06f9564eb3c28190
Chris Rau <98765332+rau03@users.noreply.github.com>
2024-11-19 19:33:48 -0600

    updated task app

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,10 +1,10 @@
 export default function sitemap() {
-  return [
-    {
-      url: "http://localhost:3000",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 1,
-    },
-  ];
+    return [
+        {
+            url: "http://localhost:3000",
+            lastModified: new Date(),
+            changeFrequency: "yearly",
+            priority: 1,
+        },
+    ];
 }
